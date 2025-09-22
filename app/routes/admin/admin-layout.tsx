9ec18bac164a590a1426f1e591d5c18c13908cf4
spanthan <spanthan@andrew.cmu.edu>
2025-09-21 20:53:56 -0400

    Implemented Dashboard Layout & UI, Auth Functionality

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,9 +1,15 @@
+import { NavItems, MobileSidebar } from "components";
 import {Outlet} from "react-router";
+import {SidebarComponent} from "@syncfusion/ej2-react-navigations"
 
 const AdminLayout = () => {
     return <div className="admin-layout"> 
-        MobileSidebar
-        <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+        <MobileSidebar/>
+        <aside className="w-full max-w-[270px] hidden lg:block">
+            <SidebarComponent width={270} enableGestures={false}>
+                <NavItems />
+            </SidebarComponent>
+        </aside>
         <aside className='children'>
             <Outlet />
         </aside>
--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,6 +1,15 @@
+import { Header } from "components"
+
 const AllUsers = () => {
     return (
-        <div> Users Table</div>
+        <main className="dashboard wrapper"> 
+            <Header 
+                title= "Trips Page"
+                description = "Check out our current users in real time"
+            />
+
+            All Users Page Contents
+        </main>
     )
 }
 
